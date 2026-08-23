1e49695e92ff16c2a50a371b17e02da57be68555
Yash <yashkesarwani8040@gmail.com>
2026-08-23 21:01:02 +0530

    LeetCode: update java/search_in_rotated_sorted_array.java

---
+++ java/search_in_rotated_sorted_array.java
@@ -0,0 +1,42 @@
+// ======================================
+// LeetCode Problem: search in rotated sorted array
+// Language: java
+// Link: https://leetcode.com/problems/search-in-rotated-sorted-array/
+// Synced by: LinkCode
+// Date: 8/23/2026, 9:01:01 PM
+// ======================================
+
+
+class Solution {
+    public int search(int[] nums, int target) {
+       int n = nums.length;
+        int low =0 ;
+        int high =n-1;
+        while(low<=high){
+            int mid = (low + high)/2;
+
+            if(nums[mid]==target) {
+                return mid;
+            
+        }
+        if (nums[low] <= nums[mid]) {
+
+                if (target>=nums[low] && target<nums[mid]) {
+                    high = mid - 1;   
+                } else {
+                    low = mid + 1;   
+                }
+
+            } else {
+
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;   
+                } else {
+                    high = mid - 1;  
+                }
+            }
+
+    }
+        return -1;
+}
+}
